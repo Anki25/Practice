4cdef9744fe89a75c04d340bac4e0aadaf6f358b
ANKITA MISHRA <mishrankita25@yahoo.com>
2016-08-05 14:11:08 +0530

    Add files via upload

---
+++ InterEx.java
@@ -0,0 +1,22 @@
+
+public class InterEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      C1 o1= new C1();
+      C2 o2= new C2();
+      C3 o3= new C3();
+      
+      o2.mul(4,5);
+      
+      Int1 o4 = new C1();
+      o4.sub(6,8);
+      
+      Int2 o5 = new C2();
+      o5.div(45,9);
+      
+      Int3 o6 = new C3();
+      o6.sayHi("Anki");
+	}
+
+}
