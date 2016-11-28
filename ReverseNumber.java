5b95138fca8c6e990c00142815ad3029bac318e7
ANKITA MISHRA <mishrankita25@yahoo.com>
2016-11-28 19:18:16 +0530

    Add files via upload

---
+++ ReverseNumber.java
@@ -0,0 +1,18 @@
+package practice;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+	
+	int x,r,n;
+	Scanner sc=new Scanner(System.in);
+	
+	public void reverse(){
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
